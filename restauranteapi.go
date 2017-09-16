893ea7619e5555d5473ed7d2aeb652732d87916a
Daniel Machado <daniellgmachado@gmail.com>
2017-09-16 21:35:35 +1000

    fully decoupled

--- restauranteapi.go
+++ restauranteapi.go
@@ -44,48 +44,7 @@ func main() {
 
 func dishlist(httpwriter http.ResponseWriter, req *http.Request) {
 
-	type ControllerInfo struct {
-		Name string
-	}
-	type Row struct {
-		Description []string
-	}
-	type DisplayTemplate struct {
-		Info       ControllerInfo
-		FieldNames []string
-		Rows       []Row
-	}
-
 	var dishlist = dishes.GetAll(mongodbvar)
 
-	items := DisplayTemplate{}
-	items.Info.Name = "Dish List"
-
-	var numberoffields = 6
-
-	// Set colum names
-	items.FieldNames = make([]string, numberoffields)
-	items.FieldNames[0] = "Name"
-	items.FieldNames[1] = "Type"
-	items.FieldNames[2] = "Price"
-	items.FieldNames[3] = "GlutenFree"
-	items.FieldNames[4] = "DairyFree"
-	items.FieldNames[5] = "Vegetarian"
-
-	// Set rows to be displayed
-	items.Rows = make([]Row, len(dishlist))
-	// items.RowID = make([]int, len(dishlist))
-
-	for i := 0; i < len(dishlist); i++ {
-		items.Rows[i] = Row{}
-		items.Rows[i].Description = make([]string, numberoffields)
-		items.Rows[i].Description[0] = dishlist[i].Name
-		items.Rows[i].Description[1] = dishlist[i].Type
-		items.Rows[i].Description[2] = dishlist[i].Price
-		items.Rows[i].Description[3] = dishlist[i].GlutenFree
-		items.Rows[i].Description[4] = dishlist[i].DairyFree
-		items.Rows[i].Description[5] = dishlist[i].Vegetarian
-	}
-
-	json.NewEncoder(httpwriter).Encode(&items)
+	json.NewEncoder(httpwriter).Encode(&dishlist)
 }
